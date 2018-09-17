5db6b71ad07861891169fa0864b97944d96a4c42
khadak.k <khadak.k@flock.com>
2018-09-17 14:56:08 +0530

    Java script refactor

--- build/js/main.js
+++ build/js/main.js
@@ -38,12 +38,13 @@ function _showEmpInput (obj) {
 function _nameValidate() {
     var name =  document.getElementById("fName").value,
         nameValidate = document.getElementById("nameValidate");
-    if (name == ""){
+    if (!name){
         nameValidate.style.display = "block";
         return false;
     }
     else {
         nameValidate.style.display = "none";
+        return true;
     }
 
 }
@@ -51,97 +52,124 @@ function _emailValidate() {
     var email =  document.getElementById("eId").value,
         emailCheck = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
         emailValidate = document.getElementById("emailValidate");
-    if (email == ""){
+    if (!email){
         emailValidate.style.display = "block";
         return false;
     }
-    else if(!emailCheck.test(email)){
-        emailValidate.textContent = "Note: Please enter a valid email id and include @ in your email id";
-        emailValidate.style.display = "block";
-        emailValidate.classList.add("input__note");
-    }
-    else {
+    else if(emailCheck.test(email)){
         emailValidate.style.display = "none";
+        return true
+    } else {
+        emailValidate.style.display = "block";
+        return false;
     }
 }
 function _salaryValidate() {
     var salary =  document.getElementById("gSalary").value,
         salaryValidate = document.getElementById("salaryValidate");
-    if (salary == ""){
+    if (!salary){
         salaryValidate.style.display = "block";
         return false;
     }
     else {
         salaryValidate.style.display = "none";
+        return true;
     }
+}
+function _investmentValidate() {
+    var investment = document.getElementById("investment").value,
+        investmentValidationElm  =document.getElementById("investmentValidation");
+
+    if(investment.length){
+        if ( parseInt(investment) <= 150000){
+            investmentValidationElm.style.display = "none";
+            return true;
+        }
+        else {
+            investmentValidationElm.style.display = "block";
+            return false;
+        }
+    }else{
+        return true;
+    }
+
 }
 function _submitEmpData () {
 
-    var name =  document.getElementById("fName").value,
-        email =  document.getElementById("eId").value,
-        salary =  document.getElementById("gSalary").value,
-        investment = document.getElementById("investment").value,
-        income =  document.getElementById("tIncome").value,
-        payable =  document.getElementById("tPayable").value;
+    var nameValidation = _nameValidate();
+    var emailValidation = _emailValidate();
+    var salaryValidation = _salaryValidate();
+    var isValidInvestmentValidation = _investmentValidate();
 
-    if (!(name || email || salary)){
-        _nameValidate();
-        _emailValidate();
-        _salaryValidate();
-        return false;
-    }
+    // if (!(name && email && salary && isValidInvestment)){
+    //     _nameValidate();
+    //     _emailValidate();
+    //     _salaryValidate();
+    //     _investmentValidate();
+    //     return false;
+    // }
 
-    var flag = false;
+    if(nameValidation && emailValidation &&  salaryValidation &&  isValidInvestmentValidation){
+
+        var name =  document.getElementById("fName").value,
+            email =  document.getElementById("eId").value,
+            salary =  document.getElementById("gSalary").value,
+            investment = document.getElementById("investment").value,
+            income =  document.getElementById("tIncome").value,
+            payable =  document.getElementById("tPayable").value,
+            flag = false;
 
 
-    for(var i=0;i<empDataArray.length; i++){
-        if(empDataArray[i].email === email){
-            flag = true;
+        for(var i=0;i<empDataArray.length; i++){
+            if(empDataArray[i].email === email){
+                flag = true;
 
-            empDataArray[i].name = name;
-            empDataArray[i].email = email;
-            empDataArray[i].salary = salary;
-            empDataArray[i].investment = investment;
-            empDataArray[i].income = income;
-            empDataArray[i].payable = payable;
+                empDataArray[i].name = name;
+                empDataArray[i].email = email;
+                empDataArray[i].salary = salary;
+                empDataArray[i].investment = investment;
+                empDataArray[i].income = income;
+                empDataArray[i].payable = payable;
 
-            _clearData();
+                _clearData();
 
+            }
         }
-    }
 
-    if(!flag){
-        var empObj = {
-            name: name,
-            email: email,
-            salary: salary,
-            investment: investment,
-            income: income,
-            payable: payable
+        if(!flag){
+            var empObj = {
+                name: name,
+                email: email,
+                salary: salary,
+                investment: investment,
+                income: income,
+                payable: payable
+            }
+            var element =  document.getElementById("employeeInput");
+            element.classList.add("js-hidden");
+
+            var element1 =  document.getElementById("addEmpButton");
+            element1.classList.add("js-hidden");
+
+            empDataArray.push(empObj);
+
+            _createEmployeeCart(empObj);
         }
-        var element =  document.getElementById("employeeInput");
-        element.classList.add("js-hidden");
-
-        var element1 =  document.getElementById("addEmpButton");
-        element1.classList.add("js-hidden");
-
-        empDataArray.push(empObj);
-
-        _createEmployeeCart(empObj);
-    }
-    else{
-        document.getElementById("employeDetails").innerHTML = "";
-        for(var j =0; j<empDataArray.length; j++){
-            _createEmployeeCart(empDataArray[j]);
+        else{
+            document.getElementById("employeDetails").innerHTML = "";
+            for(var j =0; j<empDataArray.length; j++){
+                _createEmployeeCart(empDataArray[j]);
+            }
         }
-    }
-    window.localStorage.setItem('empData', JSON.stringify(empDataArray));
+        window.localStorage.setItem('empData', JSON.stringify(empDataArray));
 
-    _clearData();
-    _hideEmployeeCart();
+        _clearData();
+        _hideEmployeeCart();
+    }
 }
+
+
 function _calculateTax() {
-    _salaryValidate();
     var salary =  document.getElementById("gSalary").value,
         investment = document.getElementById("investment").value,
         tIncome = document.getElementById("tIncome"),
@@ -149,7 +177,6 @@ function _calculateTax() {
         totalTaxAmt = 0,
         taxableIncome = salary - investment,
         isSalaryValid = (salary > 0 || investment < salary || investment <= 150000),
-        isValidInvestment = (investment <= 150000),
         isNoTaxIncome = (taxableIncome <= 250000),
         isFirstTaxSlab = (taxableIncome >= 250000) && (taxableIncome <= 500000),
         isSecondTaxSlab = (taxableIncome >= 500000) && (taxableIncome <= 1000000),
@@ -180,14 +207,9 @@ function _calculateTax() {
     else {
         document.getElementById("salaryValidate").style.display = "block";
     }
+    _salaryValidate();
+    _investmentValidate();
 
-    if (!isValidInvestment){
-        var text = "Maximum applicable investments amount is 1,50,000";
-        document.getElementById("investmentValidation").innerHTML = text;
-    }
-    else {
-        document.getElementById("investmentValidation").innerHTML = " ";
-    }
 }
 
 function _createEmployeeCart (empObj){
@@ -232,6 +254,9 @@ function _clearData() {
     document.getElementById("investment").value ="";
     document.getElementById("tIncome").value ="";
     document.getElementById("tPayable").value ="";
-    document.getElementsByClassName("input__error")[0].value ="";
+    document.getElementById("nameValidate").style.display = "none";
+    document.getElementById("emailValidate").style.display = "none";
+    document.getElementById("salaryValidate").style.display = "none";
+    document.getElementById("investmentValidation").style.display = "none";
 }
 init();
